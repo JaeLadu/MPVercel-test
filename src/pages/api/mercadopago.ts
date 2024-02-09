5c1f286ba79502f6cdccf3375a8ebcf331be1c7d
Jae <jaeladu1@gmail.com>
2024-02-09 15:09:48 -0300

    Changed imports format

--- src/pages/api/mercadopago.ts
+++ src/pages/api/mercadopago.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { getMerchantOrder } from "../../lib/mercadopago";
+import { getMerchantOrder } from "src/lib/mercadopago";
 
 export default async function handler(
    req: NextApiRequest,
